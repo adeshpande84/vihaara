3853844be75e53bb29694ddb15113a095cb1153a
Anirudh Deshpande <anirudh.deshpande1@gmail.com>
2016-09-24 13:45:51 -0600

    Use scss for styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <link href="/css/styles.css" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
         
     </head>
     <body>
